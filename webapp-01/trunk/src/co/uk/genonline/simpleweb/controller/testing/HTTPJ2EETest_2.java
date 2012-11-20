926a74131396a9bad460c21894188b263f0bb719
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2012-11-20 08:18:29 +0000

    [#73] Subversion problems after renaming folder - trying to fix!

---
+++ webapp-01/trunk/src/co/uk/genonline/simpleweb/controller/testing/HTTPJ2EETest_2.java
@@ -0,0 +1,46 @@
+package co.uk.genonline.simpleweb.controller.test;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomassecondary
+ * Date: 01/11/2012
+ * Time: 08:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class HTTPJ2EETest_2 {
+    private static TestConfiguration configuration;
+
+    @BeforeClass
+    public static void setup() {
+        configuration = new TestConfiguration(ConfigurationName.J2EE_REMOTE_TEST_1  );
+    }
+
+    @Test
+    public void editIndexTest() throws Exception {
+        TestHelper.getInstance().editIndexTest(configuration);
+    }
+    @Test
+    public void splashTest() throws Exception {
+        TestHelper.getInstance().splashPageTest(configuration);
+    }
+
+    @Test
+    public void pageContentTest() throws Exception {
+        TestHelper.getInstance().pageContentTest(configuration);
+    }
+
+    @Test
+    public void linkTest() {
+        TestHelper.getInstance().linkTest(configuration);
+    }
+
+    @Test
+    public void galleryTest() {
+        // Checks that pages do or don't have a gallery
+
+       TestHelper.getInstance().galleryTest(configuration);
+    }
+}
