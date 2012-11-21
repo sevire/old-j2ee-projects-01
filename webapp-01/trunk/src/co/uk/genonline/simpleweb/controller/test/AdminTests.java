5ff9aa57b7c46a8cf4a5a9f443291ae5f00dfd38
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2012-11-21 08:31:37 +0000

    [#73] Revert deleted files!!

---
+++ webapp-01/trunk/src/co/uk/genonline/simpleweb/controller/test/AdminTests.java
@@ -0,0 +1,11 @@
+package co.uk.genonline.simpleweb.controller.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomassecondary
+ * Date: 20/11/2012
+ * Time: 07:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class AdminTests {
+}
