8dc2b0a6304e7b24599748ae8a31574e91569b23
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2012-11-21 08:35:54 +0000

    Revert: [#73] Still re-organising

--- webapp-01/trunk/src/co/uk/genonline/simpleweb/controller/test/OtherRequestTests.java
+++ webapp-01/trunk/src/co/uk/genonline/simpleweb/controller/test/OtherRequestTests.java
@@ -1,5 +1,7 @@
-package co.uk.genonline.simpleweb.controller.test.support;
+package co.uk.genonline.simpleweb.controller.test;
 
+import co.uk.genonline.simpleweb.controller.test.support.ConfigurationName;
+import co.uk.genonline.simpleweb.controller.test.support.TestHelper;
 import org.junit.Test;
 import org.junit.runners.Parameterized;
 
