76c9bd954024f90bd8737685d09c3a6308d92028
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2012-11-21 08:26:18 +0000

    Revert: [#73] Commit after recovering deleted files during re-organisation

---
+++ webapp-01/trunk/src/co/uk/genonline/simpleweb/controller/test/support/ConfigurationName.java
@@ -0,0 +1,18 @@
+package co.uk.genonline.simpleweb.controller.test.support;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomassecondary
+ * Date: 05/11/2012
+ * Time: 18:17
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ConfigurationName {
+    STATIC_HTML_LOCAL_DEV,
+    STATIC_HTML_REMOTE_TEST_1,
+    //STATIC_HTML_REMOTE_LIVE,
+    //J2EE_LOCAL_DEV,
+    J2EE_LOCAL_TEST_1,
+    J2EE_REMOTE_TEST_1,
+    //J2EE_REMOTE_LIVE,
+}
