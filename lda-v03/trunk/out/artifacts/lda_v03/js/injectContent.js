36e5c07dc864ffba58c18b83f10e2d259632c73c
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2014-08-30 17:50:21 +0000

    [#4] Multiple Commits for a lot of changes to implement REST and Ajax

---
+++ lda-v03/trunk/out/artifacts/lda_v03/js/injectContent.js
@@ -0,0 +1,18 @@
+/**
+ * Created by thomassecondary on 25/08/2014.
+ */
+$(document).ready(function() {
+    $.ajax({
+        url: "/mistress/get/lucina.json",
+        accepts: "application/json",
+        success: function( data ) {
+            $("#status").attr("class", "error");
+            $("#status").html("Success");
+            $("#sContent").html( data.mistressContentDecoded );
+        },
+        error: function( xhr, textStatus, errorThrown) {
+            var msg = textStatus + " : " + errorThrown + " : " +  xhr.getAllResponseHeaders().toString();
+            $("#error").html(msg);
+        }
+    });
+});
