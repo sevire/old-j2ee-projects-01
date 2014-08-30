13ff8326317e03597baea30bf87fbc544287624d
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2014-08-30 17:48:06 +0000

    [#4] Multiple Commits for a lot of changes to implement REST and Ajax

--- lda-v03/trunk/src/main/java/uk/co/genonline/springapp05/controller/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains all the classes relating to the Spring compatible controllers which process user requests.
- */
-package uk.co.genonline.springapp05.controller;
--- lda-v03/trunk/src/main/java/uk/co/genonline/springapp05/model/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains all the classes which relate to business data and associated processing, including database access.
- *
- * Also contains test classes which test the model classes.
- */
-package uk.co.genonline.springapp05.model;
