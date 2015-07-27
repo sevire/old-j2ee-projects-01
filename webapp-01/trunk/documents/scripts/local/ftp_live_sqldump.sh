274ff1c5842c7b750af93231770c735081d6148a
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2015-07-27 12:20:05 +0000

    Part of massive commit after months of un-disciplined work!  Galleries, Responsive and lots of other things changed.

--- webapp-01/trunk/documents/scripts/local/ftp_live_sqldump.sh
+++ webapp-01/trunk/documents/scripts/local/ftp_live_sqldump.sh
@@ -9,4 +9,13 @@ get lda_v02.sql
 bye
 EOF
 
+# Need to stop Tomcat before applying dump or locks on database will be taken.
+# So stop and then restart afterwards...
+
+echo 'avoidance' | sudo -S /usr/local/tomcat/bin/catalina.sh stop
+
+# Now apply the dump to the development database
 /usr/local/mysql/bin/mysql -u root -pOC7AaWC77B lda_v02 < /Users/thomassecondary/Projects/webapp-01\(trunk\)/Documents/sqldump/lda_v02.sql
+
+# shouldn't need to re-enter password for restart of tomcat
+echo 'avoidance' | sudo -S /usr/local/tomcat/bin/catalina.sh start
