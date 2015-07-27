0fd663bdc7a9b475997bd38b03745caed3036dba
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2015-07-27 12:25:16 +0000

    Part of massive commit after months of un-disciplined work!  Galleries, Responsive and lots of other things changed.

---
+++ webapp-01/trunk/documents/scripts/local/applylivedump.sh
@@ -0,0 +1,13 @@
+echo NOTE! ----- This script applies an existing live dump.  It does not transfer it from the live server
+echo NOTE! ----- To ftp the live dump, use getlivedumpfromserver.sh
+
+# Need to stop Tomcat before applying dump or locks on database will be taken.
+# So stop and then restart afterwards...
+
+echo 'avoidance' | sudo -S /usr/local/tomcat/bin/catalina.sh stop
+
+# Now apply the dump to the development database
+/usr/local/mysql/bin/mysql -u root -pOC7AaWC77B lda_v02 < /Users/thomassecondary/Projects/webapp-01\(trunk\)/Documents/sqldump/lda_v02.sql
+
+# shouldn't need to re-enter password for restart of tomcat
+echo 'avoidance' | sudo -S /usr/local/tomcat/bin/catalina.sh start
