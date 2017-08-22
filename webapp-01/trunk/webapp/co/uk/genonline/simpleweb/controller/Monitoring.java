5046da4927496ea853d4dd0bac0b8421cdda223e
thomasgaylard2 <thomasgaylard2@d7c108ce-2a20-474e-bcb5-1fa8eaf03910>
2017-08-22 21:51:30 +0000

    Commit after re-creating v01.20.09.xxxx in a clean project.  Monitoring.java presumably not added to SVN in original commit of that project.  Fingers crossed this will be ok.  It worked pretty well.

---
+++ webapp-01/trunk/webapp/co/uk/genonline/simpleweb/controller/Monitoring.java
@@ -0,0 +1,31 @@
+package co.uk.genonline.simpleweb.controller;
+
+import co.uk.genonline.simpleweb.monitoring.Collator;
+import com.sun.jersey.api.provider.jaxb.XmlHeader;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import java.util.List;
+
+/**
+ * Created by thomassecondary on 04/06/2017.
+ */
+@Path("monitoring")
+public class Monitoring {
+
+    @Context
+    private ServletContext context;
+
+    @GET
+    @Path("/summary")
+    @Produces("application/xml")
+    @XmlHeader(value = "<?xml-stylesheet type='application/xml' href='/monitor-presentation.xsl' ?>")
+    public Collator monitoring() {
+        Collator collator = (Collator)context.getAttribute("monitoringCollator");
+
+        return collator;
+    }
+}
